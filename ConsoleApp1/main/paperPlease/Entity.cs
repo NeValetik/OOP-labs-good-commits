9386c34d2b8a6356c018152b858783e429ce7ecf
NeValetik <snowchug@gmail.com>
2024-10-10 22:19:01 +0300

    created the entity class and entitydata class for input entries

---
+++ ConsoleApp1/main/paperPlease/Entity.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaperPlease
+{
+    public record Entity(int id, bool? isHumanoid, string? planet, double? age, List<string>? traits);
+}
---
+++ ConsoleApp1/main/paperPlease/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using PaperPlease;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string? path = "C:\\Users\\snowc\\Desktop\\oop labs\\truLab1\\PapersPlease\\PapersPlease\\lab-papers-please\\c#-classification\\src\\main\\resources\\input.json";
+
+        string? outterpath = "C:\\Users\\snowc\\Desktop\\oop labs\\truLab1\\PapersPlease\\PapersPlease\\lab-papers-please\\c#-classification\\src\\main\\resources\\output";
+
+
+        
+    }
+}
+
+
