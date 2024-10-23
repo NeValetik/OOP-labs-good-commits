b564409f7f39e34f2db00d26983b54c7e87398bc
NeValetik <snowchug@gmail.com>
2024-10-23 19:48:36 +0300

    Task 1 with displays done

---
+++ Lab 2/Lab 2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2.Task4
+{
+    public static class Program
+    {
+        //public static void Main() {
+        //    Console.WriteLine("Hello world");
+        //}
+    }
+}
