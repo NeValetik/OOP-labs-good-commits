87e09e6674d832dbc352dfbb7ffe4f3226465614
NeValetik <snowchug@gmail.com>
2024-11-24 18:43:16 +0200

    Implemented the interfaces for dinner and refuel

--- src/queues/PriorityQueue.cs
+++ src/queues/PriorityQueue.cs
@@ -3,34 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab4.src.queues.interfaces;
 
-namespace Lab4.src
+
+namespace Lab4.src.queues
 {
-    internal class DumbQueue : IQueue
+    internal class PriorityQueue<T> : IQueue<T>
     {
-        public void Add(object item)
+        private int _currentPriority;
+        protected SortedDictionary<int, Queue<T>> priorityQueues;
+
+        public PriorityQueue()
         {
-            throw new NotImplementedException();
+            priorityQueues = new SortedDictionary<int, Queue<T>>();
+            _currentPriority = 1;
         }
 
-        public void Clear()
+        public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (!priorityQueues.ContainsKey(_currentPriority))
+                priorityQueues[_currentPriority] = new Queue<T>();
+
+            priorityQueues[_currentPriority].Enqueue(item);
+            _currentPriority++;
         }
 
-        public void Insert(int index, object item)
+        public T? Dequeue()
         {
-            throw new NotImplementedException();
+            if (priorityQueues.Count == 0)
+                return default;
+
+            var highestPriority = priorityQueues.Keys.Min();
+            var queue = priorityQueues[highestPriority];
+
+            T value = queue.Dequeue();
+            if (queue.Count == 0)
+                priorityQueues.Remove(highestPriority);
+
+            return value;
         }
 
-        public void Remove(object item)
+        public T? Peek()
         {
-            throw new NotImplementedException();
+            if (priorityQueues.Count == 0)
+                return default;
+
+            var highestPriority = priorityQueues.Keys.Min();
+            var queue = priorityQueues[highestPriority];
+
+            return queue.Peek();
         }
 
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
+        public int Count => priorityQueues.Values.Sum(q => q.Count);
     }
 }
